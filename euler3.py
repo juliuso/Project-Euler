a7760be5a2ac9ca73b7c4e34993ceed01f5c70f0
Julius O <julius.o@drexel.edu>
2011-05-10 23:06:23 -0700

    Standardized headings.

--- euler3.py
+++ euler3.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python
 
 # EULER 3
+# 
+# PROBLEM:
 # The prime factors of 13195 are 5, 7, 13, and 29.
 # What is the largest prime factor of the number: 600 851 475 143?
-
-# A prime is a number that is only divisible by 1 and itself.
-# While inefficient to start, the program should find all primes in
-# the given range and place them in a list.
-
-# Then, review each element in the list 
-
-# Primes from 1-10: 1, 3, 5, 7
+#
+# AUTHOR: jo
+# DATE:   10-MAY-2011
 
 factor = 600851475143
 n = 600851475143
@@ -21,4 +18,5 @@ while i * i < n:
         n = n / i
     i = i + 1
 	
-print "The largest prime number of " + str(factor) + str(" is ") + str(n)
+print ("The largest prime number of " + str(factor) + str(" is ")
+	+ str(n))
--- euler4.py
+++ euler4.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 
+# EULER 4
+# 
+# PROBLEM:
+# A palindromic number reads the same both ways. The largest palindrome
+# made from the product of two 2-digit numbers is 9009 = 91  99.
+#
+# Find the largest palindrome made from the product of two 3-digit numbers.
+#
+# AUTHOR: jo
+# DATE:   10-MAY-2011
+
 numbers = []
 palindromeTxt = []
 palindromeInt = []
