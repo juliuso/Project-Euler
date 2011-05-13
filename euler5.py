43a19804a28c52e989442b3b2fab8a01e1cf1cca
Julius O <julius.o@drexel.edu>
2011-05-12 23:51:05 -0700

    v.2 - Corrected to properly evaluate, and exit.

--- euler5.py
+++ euler5.py
@@ -21,34 +21,30 @@ maxBound = 20
 #for i in xrange(start, stop):
 #	numbers.append(++i)
 
-
 for i in xrange(1,math.factorial(maxBound)+1,1):
-	while len(factors) <= 1:
-		if (i%20==0
-			and i%19==0
-			and i%18==0
-			and i%17==0
-			and i%16==0
-			and i%15==0
-			and i%14==0
-			and i%13==0
-			and i%12==0
-			and i%11==0
-			and i%10==0
-			and i%9==0
-			and i%8==0
-			and i%7==0
-			and i%6==0
-			and i%5==0
-			and i%4==0
-			and i%3==0
-			and i%2==0
-			and i%1==0):
-				print i
-				factors.append(i)
-		else:
+	if (i%20==0
+		and i%19==0
+		and i%18==0
+		and i%17==0
+		and i%16==0
+		and i%15==0
+		and i%14==0
+		and i%13==0
+		and i%12==0
+		and i%11==0
+		and i%10==0
+		and i%9==0
+		and i%8==0
+		and i%7==0
+		and i%6==0
+		and i%5==0
+		and i%4==0
+		and i%3==0
+		and i%2==0
+		and i%1==0):
+			factors.append(i)
 			break
 		
-print "The factor is: " + factors
+print "The factor is: " + str(factors[0])
 
 #answer: 232792560
